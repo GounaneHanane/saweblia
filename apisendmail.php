60f6a8a62b8775a7f3f2e23f3eda6271f2e222c5
GounaneHanane <gounane.hanane@gmail.com>
2020-11-23 11:00:18 +0100

    send by sms

--- apisendmail.php
+++ apisendmail.php
@@ -21,78 +21,55 @@ $Quartier = $_POST['ville'];
 $Service = $_POST['service'];
 
 $Subject='Nouvelle inscription via le site Web';        
-$TelClientFormatted=str_replace("06","2126",$TelClient);
 
-
-     $my_apikey = "9Q8P09AF8MR9YATFY3HK"; 
-                $destination =$TelClientFormatted; 
-                $message = "Bonjour ".$NomClient.", Saweblia vous remercie pour votre demande. Conformément aux restrictions sanitaires, les réparations urgentes sont prioritaires. Aussi, les délais d'intervention peuvent être ralentis par la disponibilité de nos équipes."; 
-                $api_url = "http://panel.apiwha.com/send_message.php"; 
-                $api_url .= "?apikey=". urlencode ($my_apikey); 
-                $api_url .= "&number=". urlencode ($destination); 
-                $api_url .= "&text=". urlencode ($message); 
-                $my_result_object = json_decode(file_get_contents($api_url, false)); 
-
-                var_dump($my_result_object);
-
- // $my_apikey = "9Q8P09AF8MR9YATFY3HK"; 
- //                $destination =$TelClientFormatted; 
- //                $message = "Bonjour ".$NomClient.", Saweblia vous remercie pour votre demande. En quoi puis-je vous aider ?"; 
- //                $api_url = "http://panel.apiwha.com/send_message.php"; 
- //                $api_url .= "?apikey=". urlencode ($my_apikey); 
- //                $api_url .= "&number=". urlencode ($destination); 
- //                $api_url .= "&text=". urlencode ($message); 
- //                $my_result_object = json_decode(file_get_contents($api_url, false)); 
-/* 
-$url = 'https://api.sendgrid.com/';
-$user = 'azure_7f5b2a5cb6cfae8c1feda8d742c3f914@azure.com';
-$pass = 'p@r@d0xait1980';
-$template_id = 'ab90db9c-3993-4e38-be96-316672afcc4a';
-
-$js = array(
-  'sub' => array('-NomClient-' => array(strtoupper($NomClient)), 
-    '-Code-' => array($Code),
-    '-Quartier-' => array($Quartier_mail),
-    '-Service-' => array($Service_mail),
-    '-DateJour-' => array($DateJour),
-    '-TelClient-' => array($TelClient),
-  
-    //'-FormButton-' => array($FormButton),
-  ),
-  'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
-);
-$params = array(
-    'api_user'  => $user,
-    'api_key'   => $pass,
-    'to'        => 'saweblia@saweblia.ma',
-    'subject'   => $Subject,
-    'html'      => 'e teste',
-    'text'      => 'testing body',
-    'from'      => 'saweblia@saweblia.ma',
-    'x-smtpapi' => json_encode($js),
-  );
-
-
-$request =  $url.'api/mail.send.json';
-
-// Generate curl request
-$session = curl_init($request);
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true); */
- 
-// obtain response
-// $response = curl_exec($session);
-//Scurl_close($session); 
+///
+/// Add to database
+///
   $query = "INSERT INTO WebsiteUser(user_name, user_tel,user_email, user_quartier, user_service, user_description) 
   values('$NomClient', '$TelClient','$Email', '$Quartier', '$Service', '$Code')";
   
   $query_result = $connection->query($query);
+
+  if($Quartier=="Casablanca" || $Quartier == "Mohammédia") {
+    
+    /// 
+    /// send sms
+    ///
+
+    $curl = curl_init();
+    $PhoneNumber="+212".substr($TelClient,1);
+    $Message='Votre demande est prise en compte, un chargé de clientèle va prendre contact avec vous dans les plus brefs délais. \nMerci de télécharger notre application depuis le site : www.saweblia.ma';
+    $authorization = base64_encode('ParadoxCom:S@webli@2018'); 
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://8pz39.api.infobip.com/number/1/query",
+    CURLOPT_URL => "http://8pz39.api.infobip.com/sms/2/text/single",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{ \"from\":\"SAWEBLIA\",
+    \"to\":\"$PhoneNumber\", 
+    \"text\":\"$Message\" }",
+   CURLOPT_HTTPHEADER => array(
+   "accept: application/json",
+    "authorization: Basic $authorization",
+    "content-type: application/json"
+                ),
+              ));
+
+      $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+          }
+
+  }
 
  ?>
