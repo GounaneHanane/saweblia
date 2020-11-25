17520692a40f38e5b135505b19b0bb0e72abeb44
GounaneHanane <gounane.hanane@gmail.com>
2020-11-25 11:08:37 +0100

    contact

--- ajax.js
+++ ajax.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 });
 function SubmitFormData() {
 
+  $(".ajax-l").find( ".btn" ).remove();
     var Quartier = $('#ville').val();
         var Service = $('#service').val();
         var fname=$('#your-name').val();
@@ -10,21 +11,33 @@ function SubmitFormData() {
         var email=$('#your-email').val();
         var address=$('#ville').val();
         var service=$('#service').val();
-        
+       
      if(Quartier!="" && Service!="" && fname!="" && phone!="" && email!="" && address!="" && service!=""){
-        $.ajax({
+        $(".ajax-loader").append('<a style="margin-top:1%" class="btn btn-warning">Nous sommes entrain de traiter votre demande</a>');
+        setTimeout(function(){
+          $(".ajax-loader").find( ".btn" ).remove();
+          $('#myModal').modal('show');
+        }, 5000);
+        setTimeout(function(){
+         fname=$('#your-name').val("");
+         phone=$('#your-phone').val("");
+         email=$('#your-email').val("");
+       
+      }, 5000);
+      $.ajax({
             method: "POST",
             url: "apisendmail.php",
             data: { Quartier: Quartier, Service:Service, fname:fname, phone:phone, email:email, address:address, service:service}
           })
             .done(function( msg ) {
-              alert("Votre demande est bien transmis");
+            
             });
      }
-     else alert("Merci d'entrer tous les champs");
+     else $(".ajax-loader").append('<a style="margin-top:1%" class="btn btn-danger">Merci de renseigner tous les champs</a>');
        
 }
 function ContactUsData() {
+  $(".ajax-l").find( ".btn" ).remove();
     var Quartier = $('#city').val();
         var Service = $('#need').val();
         var fname=$('#name').val();
@@ -32,17 +45,31 @@ function ContactUsData() {
         var email=$('#email').val();
         var address=$('#city').val();
         var service=$('#service').val();
-        
+         setTimeout(function(){
+          $(".ajax-l").find( ".btn" ).remove();
+        }, 5000);
         if(Quartier!="" && Service!="" && fname!="" && phone!="" && email!="" && address!="" && service!=""){
-     
+          $(".ajax-l").append('<a style="margin-top:1%" class="btn btn-warning">Nous sommes entrain de traiter votre demande</a>');
+       
+         setTimeout(function(){
+            $(".ajax-l").find( ".btn" ).remove();
+            $('#myModall').modal('show');
+          }, 5000);
+          setTimeout(function(){
+           fname=$('#name').val("");
+           phone=$('#phone').val("");
+           email=$('#email').val("");
+         
+        }, 5000);
+        
         $.ajax({
             method: "POST",
             url: "apisendmail.php",
             data: { Quartier: Quartier, Service:Service, fname:fname, phone:phone, email:email, address:address, service:service}
           })
             .done(function( msg ) {
-              alert("Votre demande est bien transmis");
-            });  }
-            else alert("Merci d'entrer tous les champs");
+            }); }
+            else $(".ajax-l").append('<a style="margin-top:1%" class="btn btn-danger">Merci de renseigner tous les champs</a>');
+         
            
 }
--- sass/apisendmail.php
+++ sass/apisendmail.php
@@ -20,8 +20,6 @@ $Service_mail=htmlentities($_POST['Service'],ENT_COMPAT, 'UTF-8', true); //servi
 $Quartier = $_POST['Quartier'];
 $Service = $_POST['Service'];
 
- 
-
 $Subject='Nouvelle inscription via le site Web';        
 
 ///
@@ -74,43 +72,116 @@ $Subject='Nouvelle inscription via le site Web';
        
  
 
-
-
-  }
- 
 /// 
 /// add client to hubspot
 ///
-$fname=$_POST['fname'];
-$lname="";
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$Quartier=$_POST['Quartier'];
-$Service=$_POST['Service'];
+    $fname=$_POST['fname'];
+    $lname="";
+	$phone=$_POST['phone'];
+	$email=$_POST['email'];
+	$Quartier=$_POST['Quartier'];
+	$Service=$_POST['Service'];
 
 
 
-$contact_data = array(
-"fname" => $fname.' '.$Service,
-"lname" => $lname,
-"email" => $email,
-"phone" => $phone,
-"info" => "",
-"code" => "",
-"city" => "",
-"radio1" => "",
-"address" => $Quartier,
-"totalsession" =>"",
-"company" => "",
-);
+	$contact_data = array(
+		"fname" => $fname.' '.$Service,
+		"lname" => $lname,
+		"email" => $email,
+		"phone" => $phone,
+		"info" => "",
+		"code" => "",
+		"city" => "",
+		"radio1" => "",
+		"address" => $Quartier,
+		"totalsession" =>"",
+    "company" => "",
+	);
+	
+	$ans_hubspot = new ans_hubspot();
+	$ans_hubspot->contact_create($contact_data);
+	//$ans_hubspot->list_create("Recovery Lead Generation");
+	$ans_hubspot->list_assign_contact("2", $contact_data["phone"]);
+    
 
-$ans_hubspot = new ans_hubspot();
-$ans_hubspot->contact_create($contact_data);
-//$ans_hubspot->list_create("Recovery Lead Generation");
-$ans_hubspot->list_assign_contact("2", $contact_data["phone"]);
 
 
 
+  }
+  else {
+     /// 
+    /// send sms
+    ///
+
+    $curl = curl_init();
+    $PhoneNumber="+212".substr($TelClient,1);
+    $Message='Malheureusement vous n\'êtes pas dans notre zône de travail Mohémmadia/Casablanca, veuillez nous contacter pour plus d\informations. \nwww.saweblia.ma' ;
+    $authorization = base64_encode('ParadoxCom:S@webli@2018'); 
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://8pz39.api.infobip.com/number/1/query",
+    CURLOPT_URL => "http://8pz39.api.infobip.com/sms/2/text/single",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{ \"from\":\"SAWEBLIA\",
+    \"to\":\"$PhoneNumber\", 
+    \"text\":\"$Message\" }",
+   CURLOPT_HTTPHEADER => array(
+   "accept: application/json",
+    "authorization: Basic $authorization",
+    "content-type: application/json"
+                ),
+              ));
+
+      $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+          }
+       
+ 
+
+/// 
+/// add client to hubspot
+///
+    $fname=$_POST['fname'];
+    $lname="";
+	$phone=$_POST['phone'];
+	$email=$_POST['email'];
+	$Quartier=$_POST['Quartier'];
+	$Service=$_POST['Service'];
+
+
+
+	$contact_data = array(
+		"fname" => $fname.' '.$Service,
+		"lname" => $lname,
+		"email" => $email,
+		"phone" => $phone,
+		"info" => "",
+		"code" => "",
+		"city" => "",
+		"radio1" => "",
+		"address" => $Quartier,
+		"totalsession" =>"",
+    "company" => "",
+	);
+	
+	$ans_hubspot = new ans_hubspot();
+	$ans_hubspot->contact_create($contact_data);
+	//$ans_hubspot->list_create("Recovery Lead Generation");
+	$ans_hubspot->list_assign_contact("2", $contact_data["phone"]);
+    
+
+  }
 }
  class ans_hubspot{
 	private $hapikey = "73c28615-ef80-46a8-be75-98f0d01d8ad1";
